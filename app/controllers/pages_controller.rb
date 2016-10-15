cbcc380c26422c9f0b2d840f465dc22062b0a931
Jon <jonathan.on@hotmail.com>
2016-10-15 18:26:19 +0000

    User profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,12 @@ class PagesController < ApplicationController
   end
 
   def profile
+    #get username
+  if (User.find_by_username(params[:id]))  
+    @username = params[:id]
+  else
+    redirect_to root_path, :notice => "User not found!"
+  end
   end
 
   def explore
