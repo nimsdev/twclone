d6c4a9ea5a7da269933dcd88ea139793352fd74e
Jon <jonathan.on@hotmail.com>
2016-10-15 03:32:56 +0000

    second upload?

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+# controls all the code for any page inside of /pages folder
+
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
